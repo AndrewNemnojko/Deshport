cd06d1a66f4236960d285103782e09830b12a296
Andrii <106536698+AndrewNemnojko@users.noreply.github.com>
2022-11-10 16:29:37 +0200

    base response update

--- Deshport.Domain/Response/BaseResponse.cs
+++ Deshport.Domain/Response/BaseResponse.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Deshport.Domain.Enum;
 
 namespace Deshport.Domain.Response
 {
-    internal class BaseResponse
+    public class BaseResponse<T> : IBaseResponse<T>
     {
+        public string Description { get; set; }
+        public Status StatusCode { get; set; }
+        public T Data { get; set; }
+    }
+    public interface IBaseResponse<T>
+    {
+        public Status StatusCode { get; }
+        public T Data { get; }
     }
 }
